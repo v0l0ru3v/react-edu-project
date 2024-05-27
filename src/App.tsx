19cf74425c119bffd1427182e707e0ed5b147b7e
v0l0ru3vgit config --global user.name <f3ncykanal@gmail.com>
2024-05-27 13:05:53 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from './NavbarComp'
+
+const App: React.FC = () => {
+  return (
+    <NavBar />
+  );
+}
+
+export default App
---
+++ src/NavbarComp.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <a href="">ссылка</a>
+                    <a href="">ссылка</a>
+                    <a href="">ссылка</a>
+                </nav>
+            </header>
+        </>
+    )
+}
